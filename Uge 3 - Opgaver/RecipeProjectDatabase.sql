7a759a5e8c59f81865ba8ff74191b0045ffab7ff
Christian <rygerlunger@gmail.com>
2018-09-25 08:42:00 +0200

    Database

--- Uge 3 - Opgaver/RecipeProjectDatabase.sql
+++ Uge 3 - Opgaver/RecipeProjectDatabase.sql
@@ -2,62 +2,76 @@
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema recipeProject
+-- Schema recipeproject
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `User`;
-DROP TABLE IF EXISTS `Recipe`;
-DROP TABLE IF EXISTS `Image`;
+
 -- -----------------------------------------------------
--- Schema recipeProject
+-- Schema recipeproject
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `recipeproject` DEFAULT CHARACTER SET utf8 ;
 USE `recipeproject` ;
 
 -- -----------------------------------------------------
--- Table `recipeProject`.`Image`
+-- Table `recipeproject`.`User`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `recipeproject`.`Image` (
-  `id` INT NOT NULL,
-  `url` VARCHAR(200) NOT NULL,
-  PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `recipeproject`.`User` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`Id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `recipeProject`.`Recipe`
+-- Table `recipeproject`.`Recipe`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `recipeproject`.`Recipe` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  `ingredients` VARCHAR(200) NOT NULL,
-  `instructions` VARCHAR(2000) NOT NULL,
-  `image_id` INT NULL,
-  `rating` INT NULL,
+  `Description` VARCHAR(200) NOT NULL,
+  `To_do` VARCHAR(2000) NOT NULL,
+  `Cookingtime` INT NOT NULL,
+  `User_Id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `recipe_image_FK_idx` (`image_id` ASC),
-  CONSTRAINT `recipe_image_FK`
-    FOREIGN KEY (`image_id`)
-    REFERENCES `recipeproject`.`Image` (`id`)
+  INDEX `fk_Recipe_User_idx` (`User_Id` ASC),
+  CONSTRAINT `fk_Recipe_User`
+    FOREIGN KEY (`User_Id`)
+    REFERENCES `recipeproject`.`User` (`Id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `recipeProject`.`User`
+-- Table `recipeproject`.`Ingredient`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `recipeproject`.`User` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `recipe_id` INT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `users_recipes_FK_idx` (`recipe_id` ASC),
-  CONSTRAINT `users_recipes_FK`
-    FOREIGN KEY (`recipe_id`)
+CREATE TABLE IF NOT EXISTS `recipeproject`.`Ingredient` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`Id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `recipeproject`.`Recipe_has_ingredient`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `recipeproject`.`Recipe_has_ingredient` (
+  `Recipe_Id` INT NOT NULL,
+  `Ingredient_Id` INT NOT NULL,
+  `Amount` INT NOT NULL,
+  `Measure` VARCHAR(45) NOT NULL,
+  INDEX `fk_Recipe_has_ingredient_Ingredient1_idx` (`Ingredient_Id` ASC),
+  INDEX `fk_Recipe_has_ingredient_Recipe1_idx` (`Recipe_Id` ASC),
+  CONSTRAINT `fk_Recipe_has_ingredient_Ingredient1`
+    FOREIGN KEY (`Ingredient_Id`)
+    REFERENCES `recipeproject`.`Ingredient` (`Id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Recipe_has_ingredient_Recipe1`
+    FOREIGN KEY (`Recipe_Id`)
     REFERENCES `recipeproject`.`Recipe` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
